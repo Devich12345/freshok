3f2d0735b3293701c3110d2f28b2d17b6b42bcb8
Devich12345 <j.francoskoblo@gmail.com>
2021-08-10 11:56:38 +0300

    added top-slider section, benefits, product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-    
+    $('.top-slider').slick({
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrows/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrows/right.svg"></button>'
+    })
+
+    var mixer = mixitup('.product-item');
 });
